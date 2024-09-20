0ee5ce511936ec25365b9a1a646677b2d4441cc9
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2024-09-20 12:00:36 +0800

    Add files via upload

---
+++ LibrarySystemAdapter.java
@@ -0,0 +1,17 @@
+package AdapterPattern;
+
+public class LibrarySystemAdapter implements SchoolManagementApp
+{
+    private LibrarySystem librarySystem;
+
+    public LibrarySystemAdapter (LibrarySystem librarySystem)
+    {
+        this.librarySystem = librarySystem;
+    }
+
+    @Override
+    public void integrateSystem()
+    {
+        librarySystem.manageBooks();
+    }
+}
